5879225c83c0e927e29ba540851d29905e9adb86
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2026-01-27 21:16:22 +0000

    Add sharkdp/bat (via GH Releases)

---
+++ src/bat/install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-extra/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.4"
+
+# Install bat from Github Releases
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-extra/features/gh-release:1.0.25" \
+    --option repo='sharkdp/bat' --option binaryNames='bat' --option version="$VERSION" --option assetRegex='.*(.tar.gz)$'
+
+echo 'Done!'
