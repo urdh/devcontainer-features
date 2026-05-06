9b70510f9bc4feef2ea9d3edf7d89e4f9255cfb6
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2026-05-06 06:28:33 +0000

    Add `pure` option to fish feature

--- test/fish/test.sh
+++ test/fish/test.sh
@@ -4,23 +4,18 @@ set -e
 
 source /etc/os-release
 
-LATEST_FISH_VERSION="$(git ls-remote --tags https://github.com/fish-shell/fish-shell | grep -oP "[0-9]+\\.[0-9]+\\.[0-9]+" | sort -V | tail -n 1)"
-
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 # Feature-specific tests
-case "${ID}" in
-  debian|ubuntu)
-    check "fish" fish -v | grep "$LATEST_FISH_VERSION"
-  ;;
-  alpine)
-    check "fish" fish -v
-  ;;
-esac
+check "fish" fish -v
 
 if [ "${FISHER:-"true"}" = "true" ]; then
   check "fisher" fish -c "fisher -v"
+fi
+
+if [ "${PURE:-"false"}" = "true" ]; then
+  check "pure" fish -c "echo \$pure_version"
 fi
 
 # Report result
