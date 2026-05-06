609d2267e89efffa136a5d900b2e56a48c73b305
Simon Sigurdhsson <Sigurdhsson@gmail.com>
2026-05-06 12:10:45 +0200

    Use default config for `pure`

--- src/fish/pure-config.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-set -u pure_show_exit_status true
-set -u pure_convert_exit_status true
-set -u pure_show_prefix_root_prompt true
-set -u pure_symbol_virtualenv_prefix 🐍
-set -u pure_symbol_container_prefix 🐋
-set -u pure_symbol_ssh_prefix 🔗
